350ef24a14c590a75cb937aba5ca659c67c97e45
thevisad <thevisad@users.noreply.github.com>
2021-03-15 17:42:03 -0400

    Update AuthSAML.php

--- AuthSAML/AuthSAML.php
+++ AuthSAML/AuthSAML.php
@@ -2,26 +2,20 @@
 /*
  * SAML Authentication plugin for LimeSurvey
  * Copyright (C) 2013 Sixto Pablo Martin Garcia <sixto.martin.garcia@gmail.com>
- * Modified by (C) thevisad <sixto.martin.garcia@gmail.com>
-
+ * Modified by (C) thevisad <thevisad@gmail.com>
  * License: GNU/GPL License v2 http://www.gnu.org/licenses/gpl-2.0.html
- * URL: https://github.com/pitbulk/limesurvey-saml
+ * URL: https://github.com/thevisad/limesurvey-saml
  * A plugin of LimeSurvey, a free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
-
 class AuthSAML extends AuthPluginBase
 {
     protected $storage = 'DbStorage';
-
     protected $ssp = null;
-    
     static protected $description = 'SAML authentication plugin';
     static protected $name = 'SAML';
-    
     protected $settings = array(
         'simplesamlphp_path' => array(
             'type' => 'string',
@@ -73,23 +67,17 @@ class AuthSAML extends AuthPluginBase
             'label' => 'Force SAML login.',
         ),
     );
-    
     protected function get_saml_instance() {
         if ($this->ssp == null) {
-
             $simplesamlphp_path = $this->get('simplesamlphp_path', null, null, '/var/www/simplesamlphp');
-
-            // To avoid __autoload conflicts, remove limesurvey autoloads temporarily 
+            // To avoid __autoload conflicts, remove limesurvey autoloads temporarily
             $autoload_functions = spl_autoload_functions();
             foreach($autoload_functions as $function) {
                 spl_autoload_unregister($function);
             }
-
             require_once($simplesamlphp_path.'/lib/_autoload.php');
-
             $saml_authsource = $this->get('saml_authsource', null, null, 'limesurvey');
-            $this->ssp = new SimpleSAML_Auth_Simple($saml_authsource);
-
+            $this->ssp = new \SimpleSAML\Auth\Simple($saml_authsource);
             // To avoid __autoload conflicts, restote the limesurvey autoloads
             foreach($autoload_functions as $function) {
                 spl_autoload_register($function);
@@ -100,12 +88,10 @@ class AuthSAML extends AuthPluginBase
 	
 	public function init(){
         $this->storage = $this->get('storage_base', null, null, 'DbStorage');
-
         // Here you should handle subscribing to the events your plugin will handle
         $this->subscribe('beforeLogin');
         $this->subscribe('newUserSession');
         $this->subscribe('afterLogout');
-
         if (!$this->get('force_saml_login', null, null, false)) {
             $this->subscribe('newLoginForm');
         }
@@ -127,17 +113,20 @@ class AuthSAML extends AuthPluginBase
     public function afterLogout()
     {
         $ssp = $this->get_saml_instance();
-        $ssp->logout();
+        $ssp->logout('https://survey.test.gov.cfigroup.com/index.php?r=admin/authentication/sa/login');
+SimpleSAML_Session::getSessionFromRequest()->cleanup();
     }
-
+    public function beforeLogout() {
+        $ssp = $this->get_saml_instance();
+        $ssp->logout('https://survey.test.gov.cfigroup.com/index.php?r=admin/authentication/sa/login');
+		SimpleSAML_Session::getSessionFromRequest()->cleanup();
+    }
     public function newLoginForm()
     {
         $authtype_base = $this->get('authtype_base', null, null, 'Authdb');
-
         $ssp = $this->get_saml_instance();
         $this->getEvent()->getContent($authtype_base)->addContent('<li><center>Click below for CFIGroup SAML Login<br><a href="'.$ssp->getLoginURL().'" title="SAML Login"><img src="'.Yii::app()->getConfig('imageurl').'/saml_logo.gif"></a></center><br></li>', 'prepend');
     }
-
     public function getUserName()
     {
         if ($this->_username == null) {
@@ -153,14 +142,11 @@ class AuthSAML extends AuthPluginBase
         }
         return $this->_username;
     }
-
     public function getUserCommonName()
     {
         $name = '';
-
         $ssp = $this->get_saml_instance();
         $attributes = $this->ssp->getAttributes();
-
         if (!empty($attributes)) {
             $saml_name_mapping = $this->get('saml_name_mapping', null, null, 'cn');
             if (array_key_exists($saml_name_mapping , $attributes) && !empty($attributes[$saml_name_mapping])) {
@@ -169,12 +155,9 @@ class AuthSAML extends AuthPluginBase
         }
         return $name;
     }
-
-
     public function getUserMail()
     {
         $mail = '';
-
         $ssp = $this->get_saml_instance();
         $attributes = $this->ssp->getAttributes();
         if (!empty($attributes)) {
@@ -185,20 +168,16 @@ class AuthSAML extends AuthPluginBase
         }
         return $mail;
     }
-
     public function newUserSession()
     {	
         $ssp = $this->get_saml_instance();
         if ($ssp->isAuthenticated()) {
             $sUser = $this->getUserName();
             $_SERVER['REMOTE_USER'] = $sUser;
-
             $password = createPassword();
             $this->setPassword($password);
-
             $name = $this->getUserCommonName();
             $mail = $this->getUserMail();
-
             $oUser = $this->api->getUserByName($sUser);		
 			
             if (is_null($oUser))
@@ -207,16 +186,12 @@ class AuthSAML extends AuthPluginBase
                 $auto_create_users = $this->get('auto_create_users', null, null, true);
 				
                 if ($auto_create_users) {
-
                     $iNewUID = User::model()->insertUser($sUser, $password, $name, 1, $mail);
-
                     if ($iNewUID)
                     {
                         Permission::model()->insertSomeRecords(array('uid' => $iNewUID, 'permission' => Yii::app()->getConfig("defaulttemplate"),   'entity'=>'template', 'read_p' => 1));
-
                         // read again user from newly created entry
                         $oUser = $this->api->getUserByName($sUser);
-
                         $this->setAuthSuccess($oUser);
                     }
                     else {
@@ -232,14 +207,12 @@ class AuthSAML extends AuthPluginBase
                 $auto_update_users = $this->get('auto_update_users', null, null, true);
                 if ($auto_update_users) {
                     $changes = array (
-                        'full_name' => $name, 
+                        'full_name' => $name,
                         'email' => $mail,
                     );
-
                     User::model()->updateByPk($oUser->uid, $changes);
                     $oUser = $this->api->getUserByName($sUser);
                 }
-
                 $this->setAuthSuccess($oUser);
             }
         }
